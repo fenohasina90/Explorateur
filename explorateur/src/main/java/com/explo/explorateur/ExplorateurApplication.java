79e0066a8ad4a2e68db70d2a62ea3609d4c11786
Fenohasina90 <hei.fenohasina@gmail.com>
2025-12-22 15:38:39 +0300

    feat: initialisation du projet

---
+++ explorateur/src/main/java/com/explo/explorateur/ExplorateurApplication.java
@@ -0,0 +1,13 @@
+package com.explo.explorateur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplorateurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplorateurApplication.class, args);
+	}
+
+}
